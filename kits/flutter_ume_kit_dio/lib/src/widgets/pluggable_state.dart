67f72c57798e3a1541c275acdb98e8c9d8421aa6
TheodorusR <theodorus_riyanto@yahoo.com>
2025-10-28 12:03:44 +0700

    fix build flutter 3.24.3

--- kits/flutter_ume_kit_dio/lib/src/widgets/pluggable_state.dart
+++ kits/flutter_ume_kit_dio/lib/src/widgets/pluggable_state.dart
@@ -24,7 +24,7 @@ ButtonStyle _buttonStyle(
       borderRadius: BorderRadius.circular(999999),
     ),
     backgroundColor: Theme.of(context).primaryColor,
-    primary: Colors.white,
+    foregroundColor: Colors.white,
     tapTargetSize: MaterialTapTargetSize.shrinkWrap,
   );
 }
@@ -116,7 +116,7 @@ class DioPluggableState extends State<DioInspector> {
     return Material(
       color: Colors.black26,
       child: DefaultTextStyle.merge(
-        style: Theme.of(context).textTheme.bodyText2,
+        style: Theme.of(context).textTheme.bodyMedium,
         child: Align(
           alignment: Alignment.bottomCenter,
           child: Container(
@@ -139,7 +139,7 @@ class DioPluggableState extends State<DioInspector> {
                       const Spacer(),
                       Text(
                         'Dio Requests',
-                        style: Theme.of(context).textTheme.subtitle1,
+                        style: Theme.of(context).textTheme.titleLarge,
                       ),
                       Expanded(
                         child: Align(
